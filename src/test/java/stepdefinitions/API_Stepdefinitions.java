2c686e9339c8ba0b514f341ce308572561630641
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2024-04-16 15:21:42 +0200

    ilk gonderim

---
+++ src/test/java/stepdefinitions/API_Stepdefinitions.java
@@ -0,0 +1,12 @@
+package stepdefinitions;
+
+import io.restassured.path.json.JsonPath;
+import org.json.JSONObject;
+
+public class API_Stepdefinitions {
+    public static int id;
+    public static String fullPath;
+    JSONObject requestBody;
+    JsonPath jsonPath;
+
+}
