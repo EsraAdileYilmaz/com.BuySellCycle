5bce2d9b517ffd1760d0b3440e8b733150d3f24c
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2024-04-17 22:38:12 +0200

    us3tamamlandi

--- src/test/java/stepdefinitions/API_Stepdefinitions.java
+++ src/test/java/stepdefinitions/API_Stepdefinitions.java
@@ -28,18 +28,22 @@ public class API_Stepdefinitions {
    public void the_api_user_constructs_the_base_url_with_the_token(String token) {
        HooksAPI.setUpApi(token);
     }
+
     @Given("The api user sets {string} path parameters")
     public void the_api_user_sets_path_parameters(String rawPaths) {
         API_Methods.pathParamsMethod(rawPaths);
     }
+
     @Given("The API user sends a GET request and records the response from the api allCountries endpoint.")
     public void the_api_user_sends_a_get_request_and_records_the_response_from_the_api_all_countries_endpoint() {
         API_Methods.getResponse();
     }
+
     @Given("The api user verifies that the status code is {int}")
     public void the_api_user_verifies_that_the_status_code_is(Integer code) {
         API_Methods.statusCodeAssert(code);
     }
+
     @Given("The api user verifies that the message information in the response body is {string}")
     public void the_api_user_verifies_that_the_message_information_in_the_response_body_is(String message) {
         API_Methods.messageAssert(message);
@@ -58,6 +62,22 @@ public class API_Stepdefinitions {
     @Given("The API user records the response from the api allCountries endpoint, confirming that the status code is '401' and the reason phrase is Unauthenticated.")
     public void the_api_user_records_the_response_from_the_api_all_countries_endpoint_confirming_that_the_status_code_is_and_the_reason_phrase_is_unauthenticated() {
         Assert.assertTrue(API_Methods.tryCatchGet().equals(ConfigReader.getProperty("unauthorizedExceptionMessage", "api")));
+    }
+
+    @When("The API user sends a GET request and records the response from the api customer finans data endpoint.")
+    public void theAPIUserSendsAGETRequestAndRecordsTheResponseFromTheApiCustomerFinansDataEndpoint() {
+        API_Methods.getResponse();
+    }
+
+
+    @When("The api users validates to  the response body match the {string}, {string}, {string},{string},{string} information")
+    public void theApiUsersValidatesToTheResponseBodyMatchTheInformation(String wallet_running_balance, String wallet_pending_balance, String total_coupon, String total_wishlist, String total_cancel_order) {
+        jsonPath=API_Methods.response.jsonPath();
+        Assert.assertEquals(wallet_running_balance,jsonPath.getString("wallet_running_balance"));
+        Assert.assertEquals(wallet_pending_balance,jsonPath.getString("wallet_pending_balance"));
+        Assert.assertEquals(total_coupon,jsonPath.getString("total_coupon"));
+        Assert.assertEquals(total_wishlist,jsonPath.getString("total_wishlist"));
+        Assert.assertEquals(total_cancel_order,jsonPath.getString("total_cancel_order"));
     }
 
 
@@ -79,7 +99,6 @@ public class API_Stepdefinitions {
 
 
     }
-
 
 
 }
