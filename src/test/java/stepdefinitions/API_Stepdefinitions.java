91ed994becdb0ea8c37dba0fdcde15dc246ccfd7
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2024-04-17 14:10:24 +0200

    "esra degisiklikleri"

--- src/test/java/stepdefinitions/API_Stepdefinitions.java
+++ src/test/java/stepdefinitions/API_Stepdefinitions.java
@@ -1,12 +1,48 @@
 package stepdefinitions;
 
+import hooks.HooksAPI;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.restassured.path.json.JsonPath;
 import org.json.JSONObject;
+import org.junit.Assert;
+import utilities.API_Utilities.API_Methods;
 
 public class API_Stepdefinitions {
     public static int id;
     public static String fullPath;
     JSONObject requestBody;
     JsonPath jsonPath;
+  //========API Esra Baslangic===================================
+    //US_037
+   @Given("The api user constructs the base url with the {string} token.")
+   public void the_api_user_constructs_the_base_url_with_the_token(String token) {
+       HooksAPI.setUpApi(token);
+    }
+    @Given("The api user sets {string} path parameters")
+    public void the_api_user_sets_path_parameters(String rawPaths) {
+        API_Methods.pathParamsMethod(rawPaths);
+    }
+    @Given("The API user sends a GET request and records the response from the api allCountries endpoint.")
+    public void the_api_user_sends_a_get_request_and_records_the_response_from_the_api_all_countries_endpoint() {
+        API_Methods.getResponse();
+    }
+    @Given("The api user verifies that the status code is {int}")
+    public void the_api_user_verifies_that_the_status_code_is(Integer code) {
+        API_Methods.statusCodeAssert(code);
+    }
+    @Given("The api user verifies that the message information in the response body is {string}")
+    public void the_api_user_verifies_that_the_message_information_in_the_response_body_is(String message) {
+        API_Methods.messageAssert(message);
+    }
 
+    @When("The api user verifies the content of the data {int}, {string}, {string} in the response body.")
+    public void theApiUserVerifiesTheContentOfTheDataInTheResponseBody(int id, String code, String name) {
+       jsonPath=API_Methods.response.jsonPath();
+
+        Assert.assertEquals(id, jsonPath.getInt("addresses[222].id"));
+        Assert.assertEquals(code, jsonPath.getString("addresses[222].code"));
+        Assert.assertEquals(name, jsonPath.getString("addresses[222].name"));
+    }
+    //==========API Esra Sonu======================================
 }
---
+++ src/test/resources/features/API/admin/US_37/ProfileAllcountries.feature
@@ -0,0 +1,23 @@
+Feature: As an administrator, I want to be able to access all the country information registered in the system via the API connection.
+
+Scenario: When a GET request body containing valid authorization credentials is sent to the /api/profile/allCountries endpoint,
+         the returned status code should be validated as 200, and the message information in the response body should confirm as "success".
+
+  * The api user constructs the base url with the "admin" token.
+  * The api user sets "api/profile/allCountries" path parameters
+  * The API user sends a GET request and records the response from the api allCountries endpoint.
+  * The api user verifies that the status code is 200
+  * The api user verifies that the message information in the response body is "success"
+
+  @API
+ Scenario Outline:  When a GET request body containing valid authorization credentials is sent to the /api/profile/allCountries endpoint,
+                  the returned response body should be validated to confirm that the country with an ID of 223 has the following attributes: "code": "TR" and "name": "Turkey".
+
+   * The api user constructs the base url with the "admin" token.
+   * The api user sets "api/profile/allCountries" path parameters
+   * The API user sends a GET request and records the response from the api allCountries endpoint.
+   * The api user verifies the content of the data <id>, "<code>", "<name>" in the response body.
+
+   Examples:
+     | id  | code | name   |  |
+     | 223 | TR   | Turkey |  |
